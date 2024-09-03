af2c21692256bfd4644b67a1edf91ae5afe1ab26
surister <surister98@gmail.com>
2024-09-03 16:47:55 +0200

    Add referencing components

---
+++ components/content/Der.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import {useRoute} from "vue-router";
+
+const props = defineProps({
+  r: {
+    type: String,
+  },
+  link: {
+    type: String
+  }
+})
+
+const route = useRoute();
+const current_hash = ref(route.hash)
+
+watch(route, (to) => {
+  current_hash.value = to.hash
+})
+</script>
+
+<template>
+  <p :class="{highlighted: '#'+ r === current_hash, 'references': true}"
+     style="margin-top: 0; font-size: 15px">
+    <span :id="r" class="text-subtitle-1 font-weight-bold" style="color: #EF5350">[{{ r }}]</span>
+    {{ link }}
+  </p>
+</template>
+
+<style scoped>
+.highlighted {
+  background-color: rgba(239, 83, 80, 0.07);
+}
+</style>
---
+++ components/content/Ref.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import {useRoute} from "vue-router";
+
+const props = defineProps({
+  r: {
+    type: String
+  }
+})
+
+
+</script>
+
+<template>
+  <a :href="`#${r}`" @click="">
+    <sup class="text-subtitle-1 font-weight-bold" style="color: #EF5350">{{ r }}</sup>
+  </a>
+</template>
+
+<style scoped>
+
+</style>
