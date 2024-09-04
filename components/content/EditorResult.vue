fe78a9ccbbd639a6b23e2b17b3e0f1bb94b0af45
surister <surister98@gmail.com>
2024-09-04 13:27:55 +0200

    Add editorresult component

---
+++ components/content/EditorResult.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import hljs from 'highlight.js/lib/core';
+import python from 'highlight.js/lib/languages/python';
+hljs.registerLanguage('python', python);
+
+const slots = useSlots();
+const value = slots.default()[0].children.default()[0].children;
+</script>
+
+<template>
+  <v-divider thickness="3"/>
+  <span class="t text-grey-lighten-1" style="position: relative; ">
+      <code>
+        kd
+        {{ hljs.highlight(value, {language: 'python'}).value }}
+      </code>
+  </span>
+</template>
+
+<style>
+.t pre {
+  background-color: #1f2024;
+  padding: 20px;
+}
+</style>
