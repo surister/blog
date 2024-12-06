b57caf7a05b1c505c0cffcc1e2531e59bd6059ee
surister <surister98@gmail.com>
2024-12-06 11:19:57 +0100

    Add new elink (external link) component

---
+++ components/content/elink.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const props = defineProps({
+  type: {
+    type: String,
+  },
+  text: {
+    type: String
+  },
+  url: {
+    type: String
+  }
+})
+const icon = computed(() => `mdi-${props.type}`)
+</script>
+
+<template>
+  <v-btn :prepend-icon="icon"
+         size="small"
+         variant="outlined"
+         :href="url"
+         target="_blank"
+         link>
+      <span class="font-weight-bold"
+            style="font-size:15px">
+        {{ text }}
+      </span>
+  </v-btn>
+
+</template>
+
+<style scoped>
+
+</style>
