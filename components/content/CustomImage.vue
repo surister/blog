400284d5ed02b18acad264aa1a2d3b894a3d7ffe
surister <surister98@gmail.com>
2024-12-06 11:20:32 +0100

    Add CustomImage.vue component (Cannot be named Image to not clash with vuetify)

---
+++ components/content/CustomImage.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+const props = defineProps({
+  src: {
+    type: String
+  },
+  label: {
+    type: String
+  },
+  height: {
+    type: String
+  },
+  width: {
+    type: String
+  },
+  marginTop: {
+    type: String,
+    default: '0'
+  }
+})
+</script>
+
+<template>
+  <div>
+    <v-img aspect-ratio="16/9"
+           :height="height"
+           :width="width"
+           :src="src"
+           :style="{'marginTop': marginTop + 'px'}"
+           cover>
+    </v-img>
+    <v-label class="text-subtitle-2 font-italic pt-2">{{ label }}</v-label>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
