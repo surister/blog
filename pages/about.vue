d8ee3f02aafc453e98b64575b952c0c85bf5f865
surister <surister98@gmail.com>
2024-12-06 11:24:01 +0100

    Add about me page

---
+++ pages/about.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <v-container>
+    <h1 class="text-h1">Disclaimer</h1>
+    <p class="text-h4">
+      Views, opinions and information in this blog are entirely the author's, they do not
+      reflect any past, current or future employer/company/entity's views.
+    </p>
+    <v-img src="/img/cantread.png" width="500px" class="mt-5"></v-img>
+
+    <h2>Did I mentioned you or you would like to submit a correction? You can submit a Github Issue!</h2>
+
+    <h2 class="pt-10">Third person about me</h2>
+    <p class="text-h5 pt-5">[surister] believes in creating and sharing knowledge for free to
+      further science and human development in a sustainable way.</p>
+    <p class="text-h5 pt-5"> He advocates for Open Science and Open Source, having participated in
+      many related projects in the past. He also has mentored many engineers to further their careers.</p>
+    <p class="text-h5 pt-5">
+      He loves Software Engineering and enjoys diving deep into the details of things, he has worked
+      in web development, backend, mobile development and data engineering, spanning multiple
+      languages, frameworks and techniques. His favourite youtuber is 3Blue1Brown and
+      favourite programming language is Python, even though he also enjoys writing Rust, Javascript or Dart.
+    </p>
+
+    <p class="text-h5 pt-5"> He worked previously in Open Science as Senior Software Engineer @
+      Frontiers and currently works in Open-source as a Database Environment Engineer @ CrateDB</p>
+  </v-container>
+</template>
+
+<style scoped>
+
+</style>
