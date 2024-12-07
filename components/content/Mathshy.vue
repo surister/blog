319854c6cd9aaa16027f8e2a98757979cca3f086
surister <surister98@gmail.com>
2024-12-07 12:44:23 +0100

    Add Math component to display, math in katex

---
+++ components/content/Mathshy.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import 'katex/dist/katex.min.css';
+import katex from 'katex';
+import {computed} from "vue";
+
+const slots = useSlots();
+const value = slots.default()[0].children.default()[0].children;
+const bind = computed(() => katex.renderToString(value, {throwOnError: false}))
+</script>
+
+<template>
+  <div>
+    <span v-html="bind"></span>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
