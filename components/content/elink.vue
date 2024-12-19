d2d00b5e4d6dafcc95da7c8c24b287d1cd1ebf35
surister <surister98@gmail.com>
2024-12-19 20:10:44 +0100

    Improve elink

--- components/content/elink.vue
+++ components/content/elink.vue
@@ -14,17 +14,18 @@ const icon = computed(() => `mdi-${props.type}`)
 </script>
 
 <template>
-  <v-btn :prepend-icon="icon"
-         size="small"
-         variant="outlined"
-         :href="url"
-         target="_blank"
-         link>
+  <v-badge icon="mdi-open-in-new" color="white" class="mr-2">
+    <v-btn :prepend-icon="icon"
+           size="small"
+           variant="outlined"
+           target="_blank"
+           link>
       <span class="font-weight-bold"
             style="font-size:15px">
         {{ text }}
       </span>
-  </v-btn>
+    </v-btn>
+  </v-badge>
 
 </template>
 
