6f47600994b807ae2b5e6a1670bfbf7512d9dbe8
surister <surister98@gmail.com>
2024-12-20 11:19:23 +0100

    Add LineGraph component (line.vue)

---
+++ components/content/charts/line.vue
@@ -0,0 +1,68 @@
+<script setup>
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js'
+import {color} from 'chart.js/helpers';
+import {Line} from 'vue-chartjs'
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+)
+const plugin = {
+  id: 'customCanvasBackgroundColor',
+  beforeDraw: (chart, args, options) => {
+    const {ctx} = chart;
+    ctx.save();
+    ctx.globalCompositeOperation = 'destination-over';
+    ctx.fillStyle = options.color || '#ffffff';
+    ctx.fillRect(0, 0, chart.width, chart.height);
+    ctx.restore();
+  }
+};
+
+let props = defineProps({
+  chartProps: {
+    required: true
+  },
+})
+
+let config = {
+  plugins: [
+    plugin
+  ],
+  chartData: {
+    "labels": props.chartProps.labels,
+    "datasets": props.chartProps.datasets
+  },
+  chartOptions: {
+    responsive: true,
+
+    plugins: {
+      title: {}
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <Line :data="config.chartData" :options="config.chartOptions" :plugins="config.plugins"/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
