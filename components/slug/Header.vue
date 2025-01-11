2f515567a383410c82b70cf68f079c87c5b11976
surister <surister98@gmail.com>
2025-01-11 19:30:53 +0100

    Abstract Header into Header.vue component

---
+++ components/slug/Header.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+const props = defineProps(['doc'])
+</script>
+
+<template>
+  <header>
+    <v-container fluid>
+      <v-row>
+        <v-col class="v-col-lg-6 v-col-auto offset-lg-3">
+          <h1 class="text-h2 ms-n1 text-red">{{ doc.title }}</h1>
+
+          <v-row class="position-sticky bottom-0 mt-5" dense no-gutters align="center"
+                 align-content="center">
+            <v-col cols="auto" align-self="center">
+              <span class="text-subtitle-1 font-weight-bold text-grey-lighten-1">
+                29/02/2024
+              </span>
+              <span class="pl-1">·</span>
+              <elink type="comment" :text="link.name" class="mb-1"
+                     v-for="link in doc.comment_links"/>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col>
+              <p class="text-subtitle-2 text-red">Written by</p>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <template v-for="author in doc.authors">
+              <v-col>
+                <PersonCard :title="author.name" :subtitle="author.job_title"/>
+              </v-col>
+            </template>
+            <v-col></v-col>
+            <v-spacer></v-spacer>
+            <v-divider></v-divider>
+          </v-row>
+          <v-spacer></v-spacer>
+        </v-col>
+      </v-row>
+    </v-container>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
