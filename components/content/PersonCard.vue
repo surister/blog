5659082aeda10b0b1311a27bc4f7a8a75e0fb5f7
surister <surister98@gmail.com>
2025-01-11 19:30:39 +0100

    Add PersonCard

---
+++ components/content/PersonCard.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const props = defineProps({
+  title: {
+    type: String,
+  },
+  subtitle: String,
+  href: String
+})
+</script>
+
+<template>
+  <v-card
+      prepend-avatar="https://cdn.vuetifyjs.com/images/john.jpg"
+      class="mx-auto"
+      rel="noopener"
+      target="_blank"
+      max-width="300"
+      href="https://github.com/vuetifyjs/vuetify/"
+      :title="title"
+      :subtitle="subtitle"
+      variant="text"
+      density="compact"
+  ></v-card>
+</template>
+
+<style scoped>
+
+</style>
