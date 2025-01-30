168a24be69f81b6cb4906313aa0ac18dd758ba7e
surister <surister98@gmail.com>
2025-01-30 20:28:44 +0100

    Add inline katex component MaT

---
+++ components/content/MaT.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import {computed} from "vue";
+import katex from "katex";
+
+const props = defineProps(['text'])
+const bind = computed(() => katex.renderToString(props.text, {throwOnError: false}))
+</script>
+
+<template>
+  <div>
+    <span v-html="bind"></span>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
