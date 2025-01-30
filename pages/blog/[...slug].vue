2c09fcaf1ac6d2a517ec3bc637076b68e87f419d
surister <surister98@gmail.com>
2025-01-30 20:27:19 +0100

    Fix resizing

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -6,38 +6,50 @@ import Header from "~/components/slug/Header.vue";
   <ContentDoc v-slot="{ doc }">
     <article>
       <v-container fluid>
-        <Header :doc="doc"></Header>
+        <v-row>
+          <v-col class="v-col-lg-5 v-col-auto offset-lg-4">
+            <Header :doc="doc"></Header>
+          </v-col>
+        </v-row>
       </v-container>
+
       <v-container fluid>
         <v-row no-gutters>
-          <v-col class="v-col-lg-3 v-col-xl-3 v-col-xxl-3">
+          <v-col class="v-col-xxl-3 v-col-xl-3 v-col-lg-3 v-col-md-3 v-col-xs-auto v-col-sm-12">
             <div style="top: 50px" class="position-sticky">
               <v-container>
                 <Toc></Toc>
               </v-container>
             </div>
           </v-col>
-          <v-col class="v-col-lg-6 v-col-auto">
-            <main>
+          <v-col class="v-col-xl-auto v-col-lg-6 v-col-md-auto v-col-sm-12 v-col-xs-12"
+                 offset-md="1" offset-lg="1" offset-xl="1" offset-xxl="1">
+            <v-alert
+                v-if="!doc.published"
+                density="compact"
+                max-width="860"
+                text="This article is incomplete, it will most likely contain wrong data, typos, lack of references and/or unfinished paragraphs."
+                title="Warning: This is a work in progress and is not yet published."
+                type="warning"/>
+            <main style="max-width: 860px">
               <ContentRenderer :value="doc"/>
             </main>
           </v-col>
-
         </v-row>
       </v-container>
     </article>
     <v-container>
       <div class="mt-5">
-<!--        <span>-->
-<!--      <v-chip class="text-h4"-->
-<!--              style="background-color: rgba(239, 83, 80, 0.13)"-->
-<!--              variant="text"-->
-<!--              size="x-large"-->
-<!--              @click="console.log('yo')">🦧</v-chip>-->
-<!--          <v-chip class="text-h4" variant="text" size="x-large" @click="console.log('yo')">🗣🔥🔥</v-chip>-->
-<!--          <v-chip class="text-h4" variant="text" size="x-large" @click="console.log('yo')">🤔</v-chip>-->
-<!--          <v-chip class="text-h4" variant="text" size="x-large" @click="console.log('yo')">🖕</v-chip>-->
-<!--        </span>-->
+        <!--        <span>-->
+        <!--      <v-chip class="text-h4"-->
+        <!--              style="background-color: rgba(239, 83, 80, 0.13)"-->
+        <!--              variant="text"-->
+        <!--              size="x-large"-->
+        <!--              @click="console.log('yo')">🦧</v-chip>-->
+        <!--          <v-chip class="text-h4" variant="text" size="x-large" @click="console.log('yo')">🗣🔥🔥</v-chip>-->
+        <!--          <v-chip class="text-h4" variant="text" size="x-large" @click="console.log('yo')">🤔</v-chip>-->
+        <!--          <v-chip class="text-h4" variant="text" size="x-large" @click="console.log('yo')">🖕</v-chip>-->
+        <!--        </span>-->
       </div>
     </v-container>
   </ContentDoc>
@@ -52,7 +64,7 @@ a {
 }
 
 main p {
-  font-size: 25px;
+  font-size: 19px;
   margin-top: 20px;
   line-height: 30px;
 }
@@ -63,23 +75,46 @@ main h2 {
   margin-bottom: 30px;
 }
 
+main h3 {
+  margin-top: 30px;
+  margin-bottom: 30px;
+}
+main h4 {
+  margin-top: 30px;
+  margin-bottom: 30px;
+}
 
 main ul {
-  font-size: 25px;
+  font-size: 19px;
   margin-left: 50px;
   margin-top: 25px;
   margin-bottom: 25px;
 }
 
 .h {
-  font-size: 20px;
+  font-size: 19px;
   background: rgb(52, 56, 65);
-  padding: .25rem .30rem;
-  border-radius: 10px;
+  padding: .20rem .25rem;
+  border-radius: 5px;
 }
 
 #toc-title {
   font-size: 20px;
   font-weight: bold;
+}
+
+.ct table {
+  table-layout: fixed;
+  width: 100%;
+  border-collapse: collapse;
+  border: 2px solid red;
+  text-align: center;
+  text-justify: distribute;
+}
+
+
+th,
+td {
+  padding: 5px;
 }
 </style>
