1057a7fac030ec88809c8a4aeef665a0321ad399
surister <surister98@gmail.com>
2025-03-14 20:41:34 +0100

    Add group image

---
+++ components/content/alink.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const props = defineProps({
+  type: {
+    type: String,
+  },
+  text: {
+    type: String
+  },
+  url: {
+    type: String
+  },
+  color:{
+    type: String,
+    default: '#F44336'
+  }
+})
+</script>
+
+<template>
+<a :style="{'color': color}"
+   :href="url"
+   class="a-tag"
+  target="_blank">
+  {{ text }}
+</a>
+</template>
+
+<style>
+.a-tag:hover {
+  color: beige;
+}
+</style>
