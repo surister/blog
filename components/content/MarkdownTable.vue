c5fc85228c890ceaebe16b00bf792354529cfbc6
surister <surister98@gmail.com>
2025-09-14 10:45:15 +0200

    Refactor how markdown tables are parsed

--- components/content/MarkdownTable.vue
+++ components/content/MarkdownTable.vue
@@ -1,6 +1,4 @@
 <script setup>
-const slots = useSlots();
-const value = slots.default()[0].children.default()[0].children;
 const props = defineProps({
   type: {
     type: String,
@@ -30,6 +28,11 @@ const props = defineProps({
   }
 })
 
+function get_default(slots) {
+  // Gets the default content of slots as passed by nuxt in their custom components.
+  return slots.default()[0].children.default()[0].children
+}
+
 function is_row_highlighted(row_number) {
   for (const row_rules of props.rowHighlight) {
     if (row_number >= row_rules.from - 1 && row_number < row_rules.to) {
@@ -39,18 +42,55 @@ function is_row_highlighted(row_number) {
   return ""
 }
 
-function unpack_row(row){
-  return row.split("\n")[0].split("|").map((v) => v.trim()).filter((v) => v !== "")
+function parse_row(string){
+  // Given a text representing a row where values are separated by "|", returns an array
+  // with the values.
+  // Example:
+  // >>> parse_row("| one | two |", "|")
+  // ['one', 'two']
+  return string.split("\n")[0].split("|").map((v) => v.trim()).filter((v) => v !== "")
 }
 
-function get_rows(){
-  return value.split("\n").splice(2).map(unpack_row)
+function get_headers(string){
+  // Given a markdown table as a string, returns the headers as an array.
+  // Example:
+  // >>> get_headers(`
+  //      | Month    | Savings |
+  //      | -------- | ------- |
+  //      | January  | $250    |
+  //      | February | $80     |
+  //      | March    | $420    |
+  // `)
+  // ["Month", "Savings"]
+
+  return [string.split("\n")[0]].map(parse_row)[0]
+
+}
+
+function get_rows(string){
+  // Given a markdown table as a string, returns the rows as an array of arrays.
+  // Example:
+  // >>> get_rows(`
+  //      | Month    | Savings |
+  //      | -------- | ------- |
+  //      | January  | $250    |
+  //      | February | $80     |
+  //      | March    | $420    |
+  // `)
+  // [
+  //    ["January", 250],
+  //    ["February", 80],
+  //    ["March", 422]
+  // ]
+
+  // splice(2) removes the first (header row) and second (separator row)
+  return string.split("\n").splice(2).map(parse_row)
 }
 </script>
 
 <template>
   <template v-if="type === 'text'">
-    <pre>{{ value }}</pre>
+    <pre>{{ get_default($slots) }}</pre>
   </template>
   <template v-else-if="type === 'table'">
     <v-table :style="{'max-height': maxHeight + 'px'}"
@@ -61,13 +101,13 @@ function get_rows(){
              fixed-header>
       <thead>
       <tr>
-        <th v-for="header in unpack_row(value.split('\n')[0])" class="text-left">
+        <th v-for="header in get_headers(get_default($slots))" class="text-left">
           {{ header }}
         </th>
       </tr>
       </thead>
       <tbody>
-      <tr v-for="(row, index) in get_rows()" :style="{backgroundColor: is_row_highlighted(index)}">
+      <tr v-for="(row, index) in get_rows(get_default($slots))" :style="{backgroundColor: is_row_highlighted(index)}">
         <td v-for="element in row">
           {{ element }}
         </td>
